b6ec350b9a1655796cb27c668b77ce3c89fa6310
Tigran <tigran@azatyan.info>
2016-02-10 00:14:47 +0400

    improve

--- index.js
+++ index.js
@@ -3,17 +3,13 @@ var http = require('http');
 var fs = require('fs');
 var Canvas = require('canvas');
 var Url = require('url');
-var readableStream = require('readable-stream');
-
 var Image = Canvas.Image;
-
+var bg = __dirname + '/images/image.png';
+var wordperline = 6;
 var canvas = new Canvas(600, 400);
 var ctx = canvas.getContext('2d');
-var fontsize = '20';
-var wordperline = 6;
-var image = __dirname + '/images/image.png';
-
-
+    ctx.fileStyle = 'rgb(16,176,230)';
+    ctx.font = '20px Open Sans';
 /**
  * @param response
  * @param file_path
@@ -21,7 +17,6 @@ var image = __dirname + '/images/image.png';
 function serveImage(response, file_path){
     fs.createReadStream(file_path).pipe(response)
 }
-
 
 /**
  * @param request
@@ -32,9 +27,7 @@ function getText(request){
     if(typeof text == 'undefined'){
         text = 'Text not defined'
     };
-
     text = text.split(' ');
-
     var temp_array = [];
     var texts = [];
     for (var i = 0; i < text.length; i++) {
@@ -44,24 +37,18 @@ function getText(request){
             temp_array = []
         }
     };
-
     return texts;
 }
 
 var server = http.createServer(function (request, response) {
-
-    var text = getText(request);
     var name = Math.floor(Math.random() * 60000000) + 1000000;
     name = 'public/' + name + '.png';
-
-    fs.readFile(image, function (err, squid) {
+    fs.readFile(bg, function (err, squid) {
         if (err) throw err;
         var img = new Image();
         img.src = squid;
         ctx.drawImage(img, 0, 0, img.width, img.height);
         ctx.stroke();
-        ctx.font = fontsize + 'px Open Sans';
-        ctx.fillStyle = 'rgb(16,176,230)';
         var texts = getText(request);
         for (var i = 0; i < texts.length; i++) {
             ctx.fillText(texts[i], 80, 130 + (i * 40))
@@ -74,7 +61,6 @@ var server = http.createServer(function (request, response) {
         stream.on('data', function (chunk) {
             out.write(chunk)
         });
-
         stream.on('end', function () {
             serveImage(response, name)
         })
