b03e50394dd8ac01ec8b42cb151f61aade55c62b
Tigran <tigran@azatyan.info>
2016-02-14 22:20:22 +0400

    added

--- static/js/app.js
+++ static/js/app.js
@@ -3,4 +3,15 @@ jQuery(function($){
         checkboxClass: 'icheckbox_minimal-red',
         radioClass: 'iradio_minimal-red'
     });
+    $("#create").click(function(event){
+        if($("textarea").val()==''){
+            return alert('Fill the text please!')
+        } else {
+            $("#myModal").modal('show');
+            var val ="/?text="+$("textarea").val()+"&image="+$("input[name=image]:checked").val();
+            $("#image").attr("src",val);
+            $("#link").attr("href",val);
+
+        }
+    })
 });
